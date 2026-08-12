135a0f7fb861b2ce83d277ea6763a2a73245cea9
isaiahcordovadelin-wq <isaiahcordovadelin@gmail.com>
2026-08-12 14:55:50 +0800

    Activity 1

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Contact')
+
+@section('content')
+
+    <h2>Contact Us</h2>
+
+    <p>Welcome to our contact page.</p>
+
+    <p>Email: highoverride@gmail.com</p>
+    <p>Phone: 0992-068-9188</p>
+    <p>Address: Lucena City, Philippines</p>
+
+@endsection
